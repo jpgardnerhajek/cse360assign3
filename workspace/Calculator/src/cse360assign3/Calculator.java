6289689d78bdb42995de81f95777a8c1a134a8bf
James Gardner-Hajek <jay6gh@gmail.com>
2016-02-22 11:05:18 -0700

    Initial github commit message

---
+++ workspace/Calculator/src/cse360assign3/Calculator.java
@@ -0,0 +1,62 @@
+/*The Calculator class exists to act as a calculator for simple
+ * mathematical equations. It also exists as an introduction to
+ * the use of Github.
+ *@author	James Gardner-Hajek
+ */
+
+package cse360assign3;
+/* public class Calculator
+ * the core class of the program at hand.
+ */
+public class Calculator {
+
+	private int total;
+	
+	/*public Calculator()
+	 * The constructor for the Calculator class
+	 */
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	/*public int getTotal
+	 * This method is used to get the current total stored in the calculator
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	/*public void add
+	 * this method is used to add a value to the total stored value
+	 * @param value			Value to be added
+	 */
+	public void add (int value) {
+		
+	}
+	/*public void subtract
+	 * this method is used to subtract a value from the total stored value
+	 * @param value			Value to be subtracted
+	 */
+	public void subtract (int value) {
+		
+	}
+	/* public void multiply
+	 * this method is used to multiply the current total by a value
+	 * @param value			value for total to be multiplied by
+	 */
+	public void multiply (int value) {
+		
+	}
+	/*public void divide
+	 * this method is used to divide the current total by a value
+	 * @param value			value to be divided by
+	 */
+	public void divide (int value) {
+		
+	}
+	/*public String getHistory
+	 * this method is used to get the history of operations used to
+	 * obtain the current value
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ workspace/Calculator/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,52 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testCalculator() {
+		//meant to test constructor
+		Calculator calc = new Calculator();
+		assertNotNull(calc);
+	}
+	
+	@Test
+	public void testGetTotal() {
+		//meant to test getTotal
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testAdd() {
+		//meant to test add
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSubtract() {
+		//meant to test subtract
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMultiply() {
+		//meant to test multiply
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDivide() {
+		//meant to test divide
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetHistory() {
+		//meant to test getHistory
+		fail("Not yet implemented");
+	}
+
+}
