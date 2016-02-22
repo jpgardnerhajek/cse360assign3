831428b31e70c7bdfbc066afb489d0a1b832a8c3
James Gardner-Hajek <jay6gh@gmail.com>
2016-02-22 11:35:43 -0700

    Second change

--- workspace/Calculator/src/cse360assign3/Calculator.java
+++ workspace/Calculator/src/cse360assign3/Calculator.java
@@ -22,35 +22,39 @@ public class Calculator {
 	 * This method is used to get the current total stored in the calculator
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	/*public void add
 	 * this method is used to add a value to the total stored value
 	 * @param value			Value to be added
 	 */
 	public void add (int value) {
-		
+		total=total+value;
 	}
 	/*public void subtract
 	 * this method is used to subtract a value from the total stored value
 	 * @param value			Value to be subtracted
 	 */
 	public void subtract (int value) {
-		
+		total=total-value;
 	}
 	/* public void multiply
 	 * this method is used to multiply the current total by a value
 	 * @param value			value for total to be multiplied by
 	 */
 	public void multiply (int value) {
-		
+		total=total*value;
 	}
 	/*public void divide
 	 * this method is used to divide the current total by a value
 	 * @param value			value to be divided by
 	 */
 	public void divide (int value) {
-		
+		if(value==0)
+		{
+			total=0; return;
+		}	
+		total=total/value;
 	}
 	/*public String getHistory
 	 * this method is used to get the history of operations used to
--- workspace/Calculator/src/cse360assign3/CalculatorTest.java
+++ workspace/Calculator/src/cse360assign3/CalculatorTest.java
@@ -16,31 +16,53 @@ public class CalculatorTest {
 	@Test
 	public void testGetTotal() {
 		//meant to test getTotal
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(3);
+		assertTrue(calc.getTotal()==3);
 	}
 	
 	@Test
 	public void testAdd() {
 		//meant to test add
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(3);
+		calc.add(6);
+		assertTrue(calc.getTotal()==9);
 	}
 	
 	@Test
 	public void testSubtract() {
 		//meant to test subtract
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.subtract(5);
+		assertTrue(calc.getTotal()==-5);
 	}
 	
 	@Test
 	public void testMultiply() {
 		//meant to test multiply
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(4);
+		calc.multiply(3);
+		assertTrue(calc.getTotal()==12);
 	}
 	
 	@Test
 	public void testDivide() {
 		//meant to test divide
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		calc.add(6);
+		calc.divide(3);
+		assertTrue(calc.getTotal()==2);
+	}
+	
+	@Test
+	public void testDivide2() {
+		//meant to test divide by 0
+		Calculator calc = new Calculator();
+		calc.add(6);
+		calc.divide(0);
+		assertTrue(calc.getTotal()==0);
 	}
 	
 	@Test
